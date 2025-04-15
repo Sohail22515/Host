c19a7d794a436552c202d3d62c938752bfd007e1
Mohammad Sohail <118095145+Sohail22515@users.noreply.github.com>
2025-04-16 00:40:42 +0530

    Update db.js
    ssl change

--- server/config/db.js
+++ server/config/db.js
@@ -3,11 +3,8 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-
-    });
+    await mongoose.connect(MONGO_URI, { ssl: true });
+    
     console.log('✅ MongoDB connected!');
   } catch (err) {
     console.error('❌ Error connecting to MongoDB:', err);
