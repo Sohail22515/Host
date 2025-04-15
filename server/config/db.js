b0da37593f142bba8fe35b34b89611445d289290
Mohammad Sohail <118095145+Sohail22515@users.noreply.github.com>
2025-04-16 00:47:02 +0530

    Update db.js
    Logo_url

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI, { ssl: true });
+    await mongoose.connect(process.env.MONGO_URI, { ssl: true });
     
     console.log('✅ MongoDB connected!');
   } catch (err) {
